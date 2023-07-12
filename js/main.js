00573bd0f54ab18b30c8061c78a1c79ff9bb479a
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-07-12 17:15:18 +0200

    created basic block container

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
+'use strict'
+
 const playButton = document.querySelector(".btn");
-const mainContainer = document.querySelector(".main-container");
+const select = document.querySelector(".form-select");
+const container = document.querySelector(".block-container");
 
 playButton.addEventListener("click", function(){
-    mainContainer.innerHTML = "";
+    const difficulty = parseInt(select.value);
+    console.log(difficulty);
+
+
+    let blockContainer = container;
+    blockContainer.classList.replace("d-none", "d-flex");
+
+    for (i = 1; i = difficulty; i++){
+        let block = document.createElement("div");
+        block.classList.add("block");
+    }
+
     
 });
